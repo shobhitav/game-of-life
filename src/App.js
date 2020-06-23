83e1ab5376929d6c8fb073811d9c1204afd56cb3
Shobhita Vaishnav <to.shobhita@gmail.com>
2020-06-22 19:13:21 -0700

    Seeded the grid and animation started

--- src/App.js
+++ src/App.js
@@ -26,9 +26,10 @@ const width=(this.props.cols*12);
 var rowsArr=[];
 var boxClass="";
 for (var i=0;i<this.props.rows;i++){
-  for (var j=0;j<this.props.rows;j++) {
+  for (var j=0;j<this.props.cols;j++) {
     let boxId=i + "_" + j;
-
+    
+    // checking each grid ,true means on , false means off .
     boxClass=this.props.gridFull[i][j]? "box on":"box off";
     rowsArr.push(
      <Box
@@ -57,13 +58,14 @@ return(
 
 
 
-// App
+// App Component
 class App extends Component{
 constructor(){
   super();
   this.speed=100;
   this.rows=30;
   this.cols=40
+  
   this.state={
     generation:0,
     gridFull:Array(this.rows).fill().map( () => Array(this.cols).fill(false))
@@ -71,11 +73,73 @@ constructor(){
 }
 
 selectBox= (row,col) => {
+  // making copy of gridFull and then setState to update state
  let gridCopy = arrClone(this.state.gridFull);
- gridCopy[row][col] = ! gridCopy[row][col]
+ gridCopy[row][col] = !gridCopy[row][col]
  this.setState({
    gridFull: gridCopy 
- })
+ });
+}
+
+// randomly seed the grid
+seed=() => {
+  // create a copy
+  let gridCopy = arrClone(this.state.gridFull);
+  for (let i=0;i<this.rows;i++){
+    for (let j=0;j<this.cols;j++) {
+      // if randomly generated number is one , 
+     if (Math.floor(Math.random()*4) === 1){
+       gridCopy[i][j] = true;
+      }
+    }
+  }
+  this.setState({
+    gridFull: gridCopy 
+  });
+}
+
+playButton=() => {
+  // every tme play button is clicked we start over
+clearInterval(this.intervalId)
+this.intervalId =setInterval(this.play,this.speed);
+}
+
+// Play function with game of life rules  
+play=() => {
+  let g =this.state.gridFull;
+  let g2=arrClone(this.state.gridFull);
+
+  for (let i=0;i<this.rows;i++){
+    for (let j=0;j<this.cols;j++){
+      // count is the no. of neighbors
+      let count=0;
+      // checking for neighbors , if there is a neighbor , increase count
+      if (i>0) if(g[i-1][j]) count++;
+      if (i>0 && j>0) if(g[i-1][j-1]) count++;
+      if (i>0 && j<this.cols-1) if(g[i-1][j+1]) count++;
+      if(j<this.cols-1) if(g[i][j+1]) count++;
+      if(j>0) if(g[i][j-1]) count++;
+      if(i<this.rows-1) if(g[i+1][j]) count++;
+      if(i<this.rows-1 && j>0) if(g[i+1][j-1]) count++;
+      if(i<this.rows-1 && this.cols-1) if(g[i+1][j+1]) count++;
+      //if cell is alive and neighbor less than 2 or more than 3 , it dies
+      if(g[i][j] &&  (count<2 || count >3 )) g2[i][j]=false;
+      // if cell is dead and neighbor exactly 3 , it becomes a live cell
+      if (!g[i][j] && count===3) g2[i][j]=true;
+      
+    }
+  }
+
+  this.setState ({
+    gridFull: g2,
+    generation: this.state.generation +1
+  });
+
+}
+
+componentDidMount(){
+  this.seed();
+  this.playButton();
 }
 
 
@@ -95,7 +159,8 @@ return (
   )
 
 }
-}
+};
+// as it is a nested array , we have to do a deep clone like this 
 function arrClone(arr){
   return JSON.parse(JSON.stringify(arr));
 }
